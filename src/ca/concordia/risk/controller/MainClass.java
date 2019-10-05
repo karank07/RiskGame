73708385fb3aed36deb1474553d7b1a4724e4e45
Rohan Vachhani <rohan.vachhani5@gmail.com>
2019-10-04 23:53:14 -0400

    completed mainClass.java

--- src/ca/concordia/risk/controller/MainClass.java
+++ src/ca/concordia/risk/controller/MainClass.java
@@ -13,7 +13,7 @@ import ca.concordia.risk.model.Continent;
 import ca.concordia.risk.model.Country;
 
 /**
- * @author Karan
+ * @author Karan and Rohan
  *
  */
 public class MainClass {
@@ -78,6 +78,79 @@ public class MainClass {
 			br.close();
 			file.close();
 		}
+
+	}
+	
+	/**
+	* @param continentString
+	* @param ContinentList
+	*/
+	private static void stringToContinent(List<String> continentString, List<Continent> ContinentList) {
+	// TODO Auto-generated method stub
+	String[] temp = new String[3];
+
+	for (String obj : continentString) {
+
+	temp = obj.split(" ");
+	Continent objContinent = new Continent();
+	objContinent.setContinentName(temp[0]);
+
+	objContinent.setContinentControlValue(Integer.parseInt(temp[1]));
+	objContinent.setContinentColor(temp[2]);
+	ContinentList.add(objContinent);
+
+	}
+
+	for (Continent o : ContinentList) {
+	System.out.println(o.toString());
+	}
+
+	}
+
+	/**
+	* @param countryString
+	* @param CountryList
+	*/
+	private static void stringToCountry(List<String> countryString, List<Country> CountryList) {
+	// TODO Auto-generated method stub
+	String[] temp = new String[3];
+
+	for (String obj : countryString) {
+	temp = obj.split(" ");
+	Country objCountry = new Country(Integer.parseInt(temp[0]), temp[1], Integer.parseInt(temp[2]),
+	Integer.parseInt(temp[3]), Integer.parseInt(temp[4]));
+	CountryList.add(objCountry);
+	}
+
+	}
+
+	/**
+	* @param countryList
+	* @param borderString
+	*/
+	private static void setNeigbourCountry(List<Country> countryList, List<String> borderString) {
+	// TODO Auto-generated method stub
+	String[] temp2;
+	int[] temp3 = null;
+
+	int k = 0;
+	for (Country obj : countryList) {
+
+	temp2 = borderString.get(k).split(" ");
+	k = k + 1;
+	temp3 = new int[temp2.length];
+	for (int i = 0; i < temp2.length; i++) {
+	temp3[i] = Integer.parseInt(temp2[i]);
+	}
+
+	obj.setNeighbours(temp3);
+
+	}
+
+	for (Country o : countryList) {
+	System.out.println(o.toString());
+
+	}
 
 	}
 }
