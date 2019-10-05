ae14080614266d0e54f9d5e0ebf4de1773450c4d
pranal08 <38245830+pranal08@users.noreply.github.com>
2019-10-05 11:55:47 -0400

    Added model class Card,java

--- src/ca/concordia/risk/model/Card.java
+++ src/ca/concordia/risk/model/Card.java
@@ -1,5 +1,61 @@
 package ca.concordia.risk.model;
 
+/**
+ * This is a model class for Card having cardType and CountryCard as its member variables
+ * @author Pranal
+ *
+ */
+
 public class Card {
+
+	public static final String INFANTRY = "INFANTRY";
+	public static final String CAVALRY = "Cavalry";	
+	public static final String ARTILLERY = "Artillery";
+	
+	private String cardType;
+	private Country countryCard;
+	
+	/**
+	 * For creating an object for card of type cardType and for country CountryCard
+	 * @param cardType
+	 * @param countryCard
+	 */
+	public Card(String cardType, Country countryCard)
+	{
+		setCardName(cardType);
+		setCountryCard(countryCard);
+	}
+
+	/**
+	 * @return the cardType
+	 */
+	public String getCardType() {
+		return cardType;
+	}
+
+	/**
+	 * @param cardType the cardType to set
+	 */
+	public void setCardName(String cardType) {
+		if(!cardType.equalsIgnoreCase(INFANTRY) || !cardType.equalsIgnoreCase(CAVALRY) || !cardType.equalsIgnoreCase(ARTILLERY))
+			throw new IllegalArgumentException("The card type doesnot exist");
+		else
+		 this.cardType = cardType;
+	}
+
+	/**
+	 * @return the countryCard
+	 */
+	public Country getCountryCard() {
+		return countryCard;
+	}
+
+	/**
+	 * @param countryCard the countryCard to set
+	 */
+	public void setCountryCard(Country countryCard) {
+		this.countryCard = countryCard;
+	}
+	
 
 }
