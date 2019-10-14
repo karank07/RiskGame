b565e3bc31e92b172b0f9ae38f7e79858db832d1
pranal08 <38245830+pranal08@users.noreply.github.com>
2019-10-13 22:35:11 -0400

    StartUpPhase.java updated

--- src/ca/concordia/risk/controller/StartUpPhase.java
+++ src/ca/concordia/risk/controller/StartUpPhase.java
@@ -1,5 +1,3 @@
-package ca.concordia.risk.controller;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,98 +7,114 @@ import java.util.Random;
 import ca.concordia.risk.model.Country;
 import ca.concordia.risk.model.Player;
 
-
 /**
- * This class manages the startup phase of the game project and includes data members for allocation initial armies and countries
+ * This class manages the startup phase of the game project and includes data
+ * members for allocation initial armies and countries
+ * 
  * @author Pranal
  *
  */
 public class StartUpPhase {
-	
-	/**
-	 * @param numberOfPlayers stores the number of players in the game
-	 */
-	private int numberOfPlayers=0;
-	
+
 	/**
 	 * @param players maintains the list of Player entities involved the game
 	 */
 	private List<Player> players;
-	
+
 	/**
 	 * @mapInstance an instance for the Map class
 	 */
 	private ca.concordia.risk.model.Map mapInstance;
-	
+	/**
+	 * @param playerCount stores the number of players in the game
+	 */
+	static int playerCount = 0;
 	/**
 	 * 
 	 */
 	private Map<Player, List<Country>> player_country_map;
-	
+
+	static int playerTurn = 0;// place army
+
 	/**
 	 * Constructor for the StartUpPhase
+	 * 
 	 * @param numberOfPlayers stores the number of players in the game
 	 */
-	public void StartUpPhase(int numberOfPlayers) {
-		
-		mapInstance= ca.concordia.risk.model.Map.getM_instance();
-		for(int i=1; i<= numberOfPlayers; i++)
-		{
-			players.add(new Player(i,"Player" +i));
-		}
-		
-		populateCountries();
-		placeArmiesInitially();
-	}
+	public void StartUpPhase() {
 
-	private void placeArmiesInitially() {
-		
-		int j = 0;
-		int playersLeftForAssign = numberOfPlayers;
-		while (playersLeftForAssign > 0) {
-			if (players.get(j % numberOfPlayers).getPlayerTotalArmies() > 0) {
-				Player p = players.get(j%numberOfPlayers);
-				List<Country> playerCountryList = getCountriesConqueredBy(p);
-				Country randomCountry = playerCountryList.get(new Random().nextInt(playerCountryList.size()));
-				randomCountry.addCountryArmies(1);
-				p.setPlayerTotalArmies(p.getPlayerTotalArmies() - 1);
-				HashMap<String, Object> eventPayload = new HashMap<>();
-				eventPayload.put("countryName", randomCountry.getCountryName());
-				
-			} else {
-				playersLeftForAssign--;
-			}
-			j++;
-		}
-		
+		mapInstance = ca.concordia.risk.model.Map.getM_instance();
+
 	}
 
 	/**
 	 * all countries are randomly assigned to players for initial startup
 	 */
-	private void populateCountries() {
-		
+	public void populateCountries() {
+
 		int j = 0;
 		for (Country country : mapInstance.getCountries()) {
-			Player player = players.get(j % numberOfPlayers);
+			Player player = players.get(j % playerCount);
 			setNewCountryRuler(player, country, 1);
 			player.remArmies(1);
 			HashMap<String, Object> countryPopulated = new HashMap<>();
 			countryPopulated.put("countryName", country.getCountryName());
-			j++;	
+			j++;
 		}
-			
+
 	}
-		
+
+	public void placeArmyByCountryName(String counrtyName) {
+		int j = 0;
+		playerTurn++;
+		int playerId = playerTurn;
+		if (players.get(playerId).getPlayerTotalArmies() > 0) {
+			Player p = players.get(playerId);
+			Country c1 = mapInstance.getCountryByName(counrtyName);
+			List<Country> playerCountryList = getCountriesConqueredBy(p);
+			for (Country obj : playerCountryList) {
+				if (obj.equals(c1)) {
+					c1.addCountryArmies(1);
+					p.setPlayerTotalArmies(p.getPlayerTotalArmies() - 1);
+					break;
+
+				}
+			}
+
+		}
+	}
+
+	public void placeArmiesInitialRandom() {
+
+		int j = 0;
+		int playersLeftForAssign = playerCount;
+
+		while (playersLeftForAssign > 0) {
+			if (players.get(j % playerCount).getPlayerTotalArmies() > 0) {
+				Player p = players.get(j % playerCount);
+				List<Country> playerCountryList = getCountriesConqueredBy(p);
+				Country randomCountry = playerCountryList.get(new Random().nextInt(playerCountryList.size()));
+				p.setPlayerTotalArmies(p.getPlayerTotalArmies() - 1);
+				HashMap<String, Object> eventPayload = new HashMap<>();
+				eventPayload.put("countryName", randomCountry.getCountryName());
+
+			} else {
+				playersLeftForAssign--;
+			}
+			j++;
+		}
+
+	}
+
 	private boolean setNewCountryRuler(Player pl, Country co, int armies) {
 		if (co.getCountryArmy() != 0)
 			return false;
-			co.setOwnerArmy(pl.getPlayerId(),armies);
-			mapPlayerToCountry(pl, co);
-			return true;	
-			
-		}
-	
+		co.setOwnerArmy(pl.getPlayerId(), armies);
+		mapPlayerToCountry(pl, co);
+		return true;
+
+	}
+
 	public void mapPlayerToCountry(Player p, Country c) {
 		List<Country> cList = player_country_map.get(p);
 		if (cList == null) {
@@ -109,13 +123,24 @@ public class StartUpPhase {
 		}
 		cList.add(c);
 	}
-	
+
 	public List<Country> getCountriesConqueredBy(Player p) {
 		return player_country_map.get(p);
 	}
-	
-	
 
+	public void addPlayer(String playerName) {
+
+		Player p = new Player(playerCount, playerName);
+		players.add(p);
+	}
+
+	public void removePlayer(String playerName) {
+		for(Player obj:players)
+		{
+			if(playerName.contentEquals(obj.getPlayerName()))
+			{
+				players.remove(obj.getPlayerId());
+			}
+		}
+	}
 }
-
-	
