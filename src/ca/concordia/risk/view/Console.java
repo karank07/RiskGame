c1e8bfeff2c9eade2a01bd6effdeef0e8868bb1c
pranal08 <38245830+pranal08@users.noreply.github.com>
2019-11-04 14:30:11 -0500

    Added Keystrokes ENTER and ESCAPE

--- src/ca/concordia/risk/view/Console.java
+++ src/ca/concordia/risk/view/Console.java
@@ -3,6 +3,8 @@ package ca.concordia.risk.view;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
@@ -14,7 +16,7 @@ import ca.concordia.risk.controller.MainClass;
  * @author Karan
  *
  */
-public class Console implements ActionListener {
+public class Console implements ActionListener, KeyListener {
 /**
  * @param textField manages the text-field for console
  */
@@ -38,6 +40,7 @@ public class Console implements ActionListener {
 		JFrame frame = new JFrame("Console");
 		textField = new JTextField();
 		textField.setBounds(50, 100, 400, 30);
+		textField.addKeyListener(this);
 
 		submit = new JButton("Submit");
 		submit.setBounds(50, 150, 100, 30);
@@ -86,4 +89,31 @@ public class Console implements ActionListener {
 
 		JOptionPane.showMessageDialog(f, arg);
 	}
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void keyPressed(KeyEvent e) {
+
+	if(e.getKeyCode()==KeyEvent.VK_ENTER) 
+	{
+		submit.doClick();
+	}
+	
+	else if(e.getKeyCode()==KeyEvent.VK_ESCAPE)
+	{
+		exit.doClick();
+	}
+	
+}
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
