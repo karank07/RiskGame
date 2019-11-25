b75d362cffd6a9a26f1934f7df8ad91c8130c1e4
pranal08 <38245830+pranal08@users.noreply.github.com>
2019-11-24 21:34:30 -0500

    Added BenevolentStrategy

--- src/ca/concordia/risk/strategies/BenevolentStrategy.java
+++ src/ca/concordia/risk/strategies/BenevolentStrategy.java
@@ -1,5 +1,27 @@
 package ca.concordia.risk.strategies;
 
+import ca.concordia.risk.model.Country;
+import ca.concordia.risk.model.Player;
+
 public class BenevolentStrategy {
+	 public void BenevolentStrategyReinforcement(Player p) 
+	 {
+		 Country weakest = p.getWeakestCountry();
+			int reinforceArmy = p.getPlayerReinforceArmy();
+			p.reinforceArmy(weakest.getCountryName(), reinforceArmy);
+			BenevolentStrategyAttack(p);
+	 }
+
+	private void BenevolentStrategyAttack(Player p) 
+	{
+		BenevolentStrategyFortify(p);
+		
+	}
+
+	private void BenevolentStrategyFortify(Player p) {
+		
+		
+	}
+	 
 
 }
