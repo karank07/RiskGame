b5a7d3a28e6b23808046bf6069e8fe6aa0891cb6
Rohan Vachhani <rohan.vachhani5@gmail.com>
2019-11-25 16:29:45 -0500

    Updated strategy error check command

--- src/ca/concordia/risk/controller/MainClass.java
+++ src/ca/concordia/risk/controller/MainClass.java
@@ -275,9 +275,15 @@ public class MainClass {
 			}
 		}
 		int playerID = playerList.size() + 1;
-		Player p = new Player(playerID, playerName, stratergy);
-		playerList.add(p);
-		errorFlag = "false";
+		if(stratergy.equals("human") || stratergy.equals("random") || stratergy.equals("cheater") || stratergy.equals("aggressive") || stratergy.equals("benevolent")){
+			Player p = new Player(playerID, playerName, stratergy);
+			playerList.add(p);
+			errorFlag = "false";
+		}
+		else {
+			errorFlag  = "Invalid strategy type";
+		}
+		
 	}
 
 	/**
@@ -1511,9 +1517,6 @@ public class MainClass {
 			AggressiveStrategy.AggresiveStrategyReinforcement(p);
 		} else if (p.getStrategy().equals("benevolent")) {
 			BenevolentStrategy.BenevolentStrategyReinforcement(p);
-		}
-		else {
-			errorFlag = "Invalid command";
 		}
 	}
 
